22663f2e4ad05fe4267e157b97ec81e973d7819a
Jovialngandu <jovialngandu2@gmail.com>
2025-05-27 17:10:53 +0100

    [ADD] Ajout de l'utilitaire charDetector

---
+++ apps/detectors/char_detector.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import utils.image_utils as image_utils
+from ..app import App
+
+class charDetector(App):
+
+    def __init__(self,word_img,pre_name):
+        super().__init__()
+        self.word_img=word_img
+        self.pre_name=pre_name
+
+    def process(self):
+
+        img = self.word_img
+        img_copy=image_utils.copyImage(img)
+        img_gray=image_utils.imageToGray(img_copy)
+        back_color=image_utils.getBackground(img_gray)
+        kernel=np.ones((40,1),np.uint8)
+        chars_regions=[]
+        chars=[]
+
+        if back_color > 28 and back_color <200:
+            _, thresh = cv2.threshold(img_gray, 80, 255, cv2.THRESH_BINARY)
+        elif back_color>200: 
+            img_gray=cv2.bitwise_not(img_gray)
+            _, thresh = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY)
+        else:
+            _, thresh = cv2.threshold(img_gray, 128, 255, cv2.THRESH_BINARY)
+
+        
+        img_dilated=cv2.dilate(thresh,kernel,iterations=1)
+
+        contours,_=cv2.findContours(img_dilated,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+        for contour in contours:
+            chars_regions.append(cv2.boundingRect(contour))
+    
+
+        chars_regions_sorted=sorted(chars_regions,key=lambda x:x[0])
+       
+        num=0
+ 
+        for char in chars_regions_sorted:
+
+            x,y,w,h=char
+            if h>=20 and w>0.5:
+                chars.append(char)
+                num=num+1
+                roi=img_gray[y:y+h,x:x+w]
+                cv2.imwrite(f"tmp/chars/{self.pre_name}char{num}.png",roi)
+                cv2.rectangle(img_copy,(x,y),(x+w,y+h),(255,0,0),1)
+        
+        #image_utils.show(image_utils.resizeImage(img_copy,(1280,128)))
+
+
+        return {"chars":chars,"regions":chars_regions_sorted}
+
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import utils.image_utils as image_utils
 from apps.detectors.line_detector  import LineDetector
 from apps.detectors.word_detector import WordDetector
+from apps.detectors.char_detector import charDetector
 import cv2
-a=image_utils.readImage("tmp/5.png")
+a=image_utils.readImage("tmp/6.png")
 b=image_utils.copyImage(a)
 
 
@@ -15,7 +16,10 @@ _,result_line=c.process().values()
 for i in range (len(result_line)):
     #print(f"tmp/ligne/ligne{i+1}.png")
     d=WordDetector(image_utils.readImage(f"tmp/ligne/ligne{i+1}.png"),number=i+1)
-    d.process()
+    words=d.process()["words"]
+    for j in range (len(words)):
+        c=charDetector(word_img=image_utils.readImage(f"tmp/words/l{i+1}word{j+1}.png"),pre_name=f"l{i+1}word{j+1}")
+        c.process()
 
 
 #image_utils.show(b)
