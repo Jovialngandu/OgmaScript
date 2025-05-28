a9fe8d466b3aafa78c11ed10be47c72de26fad73
Jovialngandu <jovialngandu2@gmail.com>
2025-05-28 18:30:40 +0100

    [UPDATE] Test sur la charDetector

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import utils.image_utils as image_utils
+import utils.ogma_utils as ogma_utils
 from apps.detectors.line_detector  import LineDetector
 from apps.detectors.word_detector import WordDetector
 from apps.detectors.char_detector import charDetector
+
 import cv2
-a=image_utils.readImage("tmp/6.png")
+a=image_utils.readImage("tmp/1.png")
 b=image_utils.copyImage(a)
 
 
@@ -19,8 +21,12 @@ for i in range (len(result_line)):
     words=d.process()["words"]
     for j in range (len(words)):
         c=charDetector(word_img=image_utils.readImage(f"tmp/words/l{i+1}word{j+1}.png"),pre_name=f"l{i+1}word{j+1}")
-        c.process()
+        f=c.process()
+        rois=f["rois"]
 
+        for  k in range(len(rois)):
+            #image_utils.show(rois[k]["roi"])
+            ogma_utils.ogmaCharPreprocessing(img=rois[k]["roi"],char_name=rois[k]["name"])
 
 #image_utils.show(b)
 
